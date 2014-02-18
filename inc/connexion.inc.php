0a0e3e42a03634c6da11e0eb29891fefdeac21c1
Blag <blag001@users.noreply.github.com>
2014-02-18 11:14:34 +0100

    MAJ commentaire / ajout FALSE dans connexion.inc

--- inc/connexion.inc.php
+++ inc/connexion.inc.php
@@ -11,12 +11,12 @@
 	 * 		$mdp        = '';
 	 * 		$production = false;
 	 *
-	 * @param string $host l'host a utiliser (localhost by def)
+	 * @param string $host l'host a utiliser (localhost par defaut)
 	 * @param string $db_name nom de la base de donnee
 	 * @param string $user utilisateur de la BDD
 	 * @param string $mdp mot de passe de l'utilisateur
-	 * @param string $production (des)active les messages d'erreurs
+	 * @param bool $production (des)active les messages d'erreurs
 	 */
 
 if (empty($_SESSION['bdd']))
-	$_SESSION['bdd'] = new Bdd(null, null, 'root', '');
+	$_SESSION['bdd'] = new Bdd(null, null, 'root', '', false);
