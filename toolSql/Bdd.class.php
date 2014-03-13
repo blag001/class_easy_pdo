32b1ff3d7180588bcab9fba2824af124794c027a
blag <blag@rakshata.com>
2014-03-13 16:19:01 +0100

    add prepare/execute method

--- toolSql/Bdd.class.php
+++ toolSql/Bdd.class.php
@@ -20,11 +20,26 @@
 	 * 		integer = exec(string $sql[, array $arg])
 	 * ```
 	 *
-	 * execute une commande et retourne le **nombre de lignes** affectées
+	 * *Méthodes avancées (facultatives) pour optimiser certaines requêtes*
+	 *
+	 * - method prepare() :
+	 * ```php
+	 * 		bool = prepare(string $sql)
+	 * ```
+	 *
+	 * prepare une requête pour être exécuté plusieurs fois
+	 *
+	 * - method execute() :
+	 * ```php
+	 * 		array|object|integer = execute([array $arg[, int $retour]])
+	 * ```
+	 *
+	 * (re)execute une requete avec les paramètres demandés
 	 *
 	 * @global boolean SINGLE_RES
+	 * @global int NO_RES
 	 * @author Benoit <benoitelie1@gmail.com>
-	 * @version v.4.0.3
+	 * @version v.5.0.0
 	 * @link https://github.com/blag001/class_easy_pdo depot GitHub
 	 */
 class Bdd
@@ -43,9 +58,13 @@ class Bdd
 
 		/** @var PDO variable avec l'instance PDO */
 	private $oBdd  = null;
+		/** @var object variable qui contient une requête préparée */
+	private $oReq  = null;
 
 		/** constante à utiliser en cas de resultat unique attendu */
 	const SINGLE_RES = true;
+		/** constante à utiliser avec prepare/execute si il n'y a pas de résultat attendu */
+	const NO_RES = -1;
 
 	////////////////////
 	// CONSTRUCTEUR  //
@@ -86,6 +105,11 @@ class Bdd
 		/** variables a sauver a la fin du chargement de page */
 	public function __sleep()
 	{
+		if (is_object($this->oReq)) {
+				// on ferme la requete en cours
+			$this->oReq->closeCursor();
+		}
+
 		return array('host', 'db_name', 'user', 'mdp', 'production');
 	}
 
@@ -233,12 +257,13 @@ class Bdd
 	public function query($sql, array $arg = null, $mono_line = false)
 	{
 		try {
+				// erreur si on passe autre chose qu'une string
+			if(!is_string($sql))
+				throw new InvalidArgumentException(__METHOD__.'(): String only: '.ucfirst(gettype($sql)).' found in parametre \'$sql = '.strtoupper(gettype($sql)).'\'' );
+
 				// on regarde si on a des variables en arguments
 			if(!empty($arg))
 			{
-					// erreur si on passe autre chose qu'une string
-				if(!is_string($sql))
-					throw new InvalidArgumentException(__METHOD__.'(): String only: '.ucfirst(gettype($sql)).' found in parametre \'$sql = '.strtoupper(gettype($sql)).'\'' );
 					// on prepare la requete SQL
 				$req = $this->oBdd->prepare($sql);
 					// on lie les elements a la requete
@@ -264,9 +289,6 @@ class Bdd
 			}
 			else
 			{
-					// erreur si on passe autre chose qu'une string
-				if(!is_string($sql))
-					throw new InvalidArgumentException(__METHOD__.'(): String only: '.ucfirst(gettype($sql)).' found in parametre \'$sql = '.strtoupper(gettype($sql)).'\'' );
 					// on fait une query simple
 				$req = $this->oBdd->query($sql);
 			}
@@ -328,18 +350,19 @@ class Bdd
 		 * @api
 		 *
 		 * @param  string $sql la requete SQL a executer
-		 * @param  array $arg l'array avec les parametres
+		 * @param  array $arg facultatif : l'array avec l(es) parametre(s)
 		 * @return int le nombre de ligne affectee
 		 */
 	public function exec($sql, array $arg = null)
 	{
 		try {
+				// erreur si on passe autre chose qu'une string
+			if(!is_string($sql))
+				throw new InvalidArgumentException(__METHOD__.'(): String only: '.ucfirst(gettype($sql)).' found in parametre \'$sql = '.strtoupper(gettype($sql)).'\'' );
+
 				// on regarde si on a des variables en arguments
 			if(!empty($arg))
 			{
-					// erreur si on passe autre chose qu'une string
-				if(!is_string($sql))
-					throw new InvalidArgumentException(__METHOD__.'(): String only: '.ucfirst(gettype($sql)).' found in parametre \'$sql = '.strtoupper(gettype($sql)).'\'' );
 					// on prepare la requete SQL
 				$req = $this->oBdd->prepare($sql);
 					// on lie les elements a la requete
@@ -370,14 +393,181 @@ class Bdd
 				$req->closeCursor();
 			}
 			else{
-					// erreur si on passe autre chose qu'une string
-				if(!is_string($sql))
-					throw new InvalidArgumentException(__METHOD__.'(): String only: '.ucfirst(gettype($sql)).' found in parametre \'$sql = '.strtoupper(gettype($sql)).'\'' );
 					// si pas de variable, on fait une requete simple
 				$out = $this->oBdd->exec($sql);
 			}
 
 			return $out;
+		}
+			// gestion des erreurs
+		catch (Exception $e) {
+			$this->_showError($e);
+		}
+	}
+
+		/**
+		 * prepare une requête pour une/des execution(s) ultérieure
+		 *
+		 * **AVANCÉE : Cette methode permet des gains de performance mais vous pouvez utiliser `->query()` et `->exec()` à la place**
+		 *
+		 * *Cette methode implique l'utilisation de la methode `->execute()`.
+		 * Sans appelle à celle-ci, aucune action ne sera réalisée.*
+		 *
+		 * Utilisez cette methode dans les cas où vous auriez à réaliser plusieurs appelles d'une même requête sur la même page,
+		 * mais en devant changer les paramètres à lui passer.
+		 *
+		 * *Attention, vous ne pouvez préparer qu'une requête à la fois, un nouveau `->prepare()` écraseras le précédent*
+		 *
+		 * @api
+		 *
+		 * @param  string $sql la requête SQL a préparer
+		 * @return bool      retourne un TRUE/FALSE sur la preparation de la requête
+		 */
+	public function prepare($sql=null)
+	{
+		try {
+				// erreur si on passe autre chose qu'une string
+			if(!is_string($sql))
+				throw new InvalidArgumentException(__METHOD__.'(): String only: '.ucfirst(gettype($sql)).' found in parametre \'$sql = '.strtoupper(gettype($sql)).'\'' );
+
+				// on prepare la requete SQL
+			$this->oReq = $this->oBdd->prepare($sql);
+
+			return !empty($this->oReq);
+		}
+			// gestion des erreurs
+		catch (Exception $e) {
+			$this->_showError($e);
+		}
+	}
+		/**
+		 * execute une requête préparé à l'aide de `->prepare()`
+		 *
+		 * **AVANCÉE : Cette methode permet des gains de performance mais vous pouvez utiliser `->query()` et `->exec()` à la place**
+		 *
+		 * *Vous devez avant avoir préparé une requête à l'aide de `->prepare()`,
+		 * Vous pouvez appeller autemps de fois que vous avez besoin la méthode `->execute()`.*
+		 *
+		 * Les arguments à passer devrais changer à chaque appelle pour exploiter pleinement cette méthode.
+		 *
+		 * Retourne :
+		 * - soit **un integer** du nombre de ligne affectée si $retour a `Bdd::NO_RES` (ou -1),
+		 * - soit **un objet** si $retour a `Bdd::SINGLE_RES` (ou TRUE),
+		 * - soit **un array d'objet** si $retour a FALSE ou NULL (par defaut)
+		 *
+		 * On lui passe en 1er parametre les arguments dans un tableau (aussi nommé array).
+		 * L'array doit etre associatif `marqueur => valeur`
+		 * ```php
+		 * 		array('mon_marqueur' => $maVariable);
+		 * 		array('marqueur1' => $var1, 'marqueur2'=> $var2);
+		 * ```
+		 *
+		 * Si vous savez que vous allez avoir un seul resultat
+		 * (par ex, un `COUNT(*)`, un `getUn...()` )
+		 * utilisez en 2nd parametre de execute() `Bdd::SINGLE_RES` (ou TRUE)
+		 * la methode vous retourneras directement un **objet**
+		 *
+		 * Si vous n'attendais pas de retour
+		 * (par ex, un  `DELETE`, `INSERT INTO`, `UPDATE`) )
+		 * utilisez en 2nd parametre de execute() `Bdd::NO_RES` (ou -1)
+		 * la methode vous retourneras directement un **integer** du nombre de ligne affectée
+		 *
+		 * La requete prend donc ces formes :
+		 * ```php
+		 * 			// compter un nombre de ligne pour des types définis
+		 * 		$sql  = 'SELECT COUNT(*) AS `alias_nombre` FROM `maTable` WHERE `tab_type` = :type';
+		 * 		$_SESSION['bdd']->prepare($sql);
+		 * 		$oNbType1 = $_SESSION['bdd']->execute(
+		 * 			array('type'=>$unType) ,
+		 * 			Bdd::SINGLE_RES );
+		 * 		$oNbType2 = $_SESSION['bdd']->execute(
+		 * 			array('type'=>$unAutreType) ,
+		 * 			Bdd::SINGLE_RES );
+		 *
+		 * 			// chercher des lignes à plusieurs date
+		 * 		$sql  = 'SELECT * FROM `maTable` WHERE `tab_date` BETWEEN :start AND :end LIMIT :nb_total';
+		 * 		$_SESSION['bdd']->prepare($sql);
+		 * 		$lignesDate1 = $_SESSION['bdd']->execute(
+		 * 			array(
+		 * 				'start'=> $debutDate1,
+		 * 				'end'=> $finDate1,
+		 * 				'nb_total'=> intval($nb_total),
+		 * 				)
+		 * 			);
+		 * 		$lignesDate2 = $_SESSION['bdd']->execute(
+		 * 			array(
+		 * 				'start'=> $debutDate2,
+		 * 				'end'=> $finDate2,
+		 * 				'nb_total'=> intval($nb_total),
+		 * 				)
+		 * 			);
+		 * ```
+		 *
+		 * *Attention, pour les `LIMIT` il faut forcer la variable en integer, via `intval()`*
+		 *
+		 * En cas de retour (autre que `Bdd::NO_RES` donc),
+		 * on recupère les valeurs en utilisent le **nom de la colonne** dans la table (ou l'alias via `AS mon_alias`)
+		 * - Dans le cas du `Bdd::SINGLE_RES`, on a directement un **objet** dans data :
+		 * ```php
+		 * 		echo $data->tab_colonne_1;
+		 * 		echo $data->mon_alias;
+		 * ```
+		 * - Sinon il faut faire une boucle dans le tableau (**array**) :
+		 * ```php
+		 * 		foreach($data as $unObjet){
+		 * 			echo $unObjet->tab_colonne_2;
+		 * 		}
+		 * ```
+		 *
+		 * @api
+		 *
+		 * @param  array  $arg       facultatif : l(es) argument(s) à passer
+		 * @param  int    $retour    facultatif : demande un resultat en objet, array d'objet, ou pas de résultat
+		 * @return array|object|integer             retourne un objet, un tableau (array) d'objets ou le nombre de ligne affectée
+		 */
+	public function execute(array $arg = null, $retour = false)
+	{
+		try {
+				// erreur si on a pas préparé d'objet
+			if(!is_object($this->oReq))
+				throw new InvalidArgumentException(__METHOD__.'(): No prepared statement found ! You should call the ->prepare() method before execute with this :<br />$arg = <br><pre>'.htmlspecialchars(var_dump($sql)).'</pre>' );
+
+				// on regarde si on a des variables en arguments
+			if(!empty($arg))
+			{
+					// on lie les elements a la requete
+				foreach ($arg as $key => &$value){
+						// erreur si on passe un objet
+					if(is_object($value))
+						throw new InvalidArgumentException(__METHOD__.'(): String or Integer only: Object found in parametre \'array("'.$key.'"=>OBJECT,...)\'' );
+						// erreur si on passe un array
+					elseif(is_array($value))
+						throw new InvalidArgumentException(__METHOD__.'(): String or Integer only: Array found in parametre \'array("'.$key.'"=>ARRAY,...)\'' );
+						// on regarde si il y a type integer a forcer
+					elseif(is_int($value))
+						$this->oReq->bindParam($key, $value, PDO::PARAM_INT);
+						// sinon on met en string
+					else
+						$this->oReq->bindParam($key, $value, PDO::PARAM_STR);
+				}
+					// on evite les bug lie a la reference
+				unset($value);
+			}
+
+				// on execute la requete
+			$out = $this->oReq->execute();
+				// si on a une erreur dans le retour
+			if(!$out)
+				throw new RuntimeException(__METHOD__.'(): Error found : executed with this :<br />$arg = <br><pre>'.htmlspecialchars(var_dump($sql)).'</pre>' );
+
+				// si pas de retour demandé (NO_RES)
+			if($retour == -1)
+				return $this->oReq->rowCount(); // nombre de ligne affectee
+				// si on demande une mono-ligne, un simple fetch
+			elseif($retour)
+				return $this->oReq->fetch();
+			else // sinon on cherche tous les OBJET dans un ARRAY
+				return $this->oReq->fetchAll();
 		}
 			// gestion des erreurs
 		catch (Exception $e) {
